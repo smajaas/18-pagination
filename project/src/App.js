ccc926183ca31d6afee3ee31b69c97cc50a0d6f9
smajaas <smajaas@gmail.com>
2023-06-21 20:48:50 +0530

    pagination button and prev and next button

--- project/src/App.js
+++ project/src/App.js
@@ -9,12 +9,35 @@ function App() {
   useEffect(() => {
     if (loading) return;
     setFollowers(data[page]);
-  }, [loading]);
+  }, [loading, page]);
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
 
   return (
     <main>
       <div className='section-title'>
-        <h1>{loading ? 'Loading...' : 'pagination'}</h1>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
         <div className='underline'></div>
       </div>
       <section className='followers'>
@@ -23,6 +46,27 @@ function App() {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
+        {!loading && (
+          <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? 'active-btn' : null}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className='next-btn' onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
       </section>
     </main>
   );
